620ffac1d3b10551504caeccf80a5ee155650644
SiekeMIS <lucasrosas.991402@gmail.com>
2024-12-30 14:18:46 -0300

    modifique la tabla, me centré principalmente en sql, depsues veo los views

--- GestionTareas/tareas/views.py
+++ GestionTareas/tareas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+from tareas.models import Tarea
+
+Tarea.objects.create(nombre="Tarea 1", descripcion="Descripción de la tarea 1", estado="no_empezado", prioridad="alta")
